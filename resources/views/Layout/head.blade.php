10976cb93f2176e78f0e5fd032c5c56eb34da3cf
Erjon Kuka <suada.kumrija@fshnstudent.info>
2020-04-26 22:00:40 +0200

    ndryshimi i emrit te title

--- resources/views/Layout/head.blade.php
+++ resources/views/Layout/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta name="description" content="">
-<meta name="Suada" content="Blade">
-<title>Checkout our layout</title>
+<meta name="Book" content="Blade">
+<title>Books O'Clock</title>
 <!-- load bootstrap from a cdn -->
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
