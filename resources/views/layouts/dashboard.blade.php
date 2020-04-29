3c64c69af2e70736bf3dafc1ad1ebeedd6b2dca2
Erjon Kuka <suada.kumrija@fshnstudent.info>
2020-04-29 22:56:31 +0200

    ndryshime te vogla

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -31,23 +31,35 @@
 <body>
 
 <div class="container">
-    <h2 style="margin-top: 12px;" class="alert alert-success">Books O'Clock
-    </h2><br>
-    <div class="row" style="clear: both;margin-top: 18px;">
-        <div class="col-12 text-right">
-            <a href="/login" class="navbar-brand">
-                Laravel Custom Auth
-            </a> <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
-            <div id="" class="collapse navbar-collapse">
-                <ul class="navbar-nav mr-auto"></ul>
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><a href="/login" class="nav-link">Login</a></li>
-                    <li class="nav-item"><a href="/register" class="nav-link">Register</a></li>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Book O'Clock</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <ul class="navbar-nav mr-auto">
+
+                            <li class="nav-item active">
+                                <a  class="nav-link" href="{{ url('/custom-login') }}">Login</a>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="{{ url('/custom-register') }}">Register</a>
+                            </li>
                 </ul>
-            </div>
+            </form>
         </div>
-    </div>
-    <div class="row">
+    </nav>
+
+    <div>
+
                 @yield('content')
     </div>
 </div>
