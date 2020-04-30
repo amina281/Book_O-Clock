2707023a04893b7e9786569e7504fe5fb03a0d52
Suada <suada.kumrija@fshnstudent.info>
2020-04-30 23:57:54 +0200

    sistemim

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Route::get('/register', 'RegisterController@index');
 Route::post('/register', 'RegisterController@store');
 
+
 Route::get('/login', 'LoginController@index');
 Route::post('/login', 'LoginController@store');
 Route::get('/logout',  'LoginController@logout');
