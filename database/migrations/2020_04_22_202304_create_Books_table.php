99ff7a8f9f48f3ce30303e9f572bbcce3178bed3
Amina <amina.mehmeti@fshnstudent.info>
2020-05-16 12:39:32 +0300

    books category

--- database/migrations/2020_04_22_202304_create_Books_table.php
+++ database/migrations/2020_04_22_202304_create_Books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBooksTable extends Migration
+class CreateProductsTable extends Migration
 {
 
     public function up()
@@ -29,6 +29,6 @@ class CreateBooksTable extends Migration
 
     public function down()
     {
-        Schema::dropIfExists('Books');
+        Schema::dropIfExists('products');
     }
 }
