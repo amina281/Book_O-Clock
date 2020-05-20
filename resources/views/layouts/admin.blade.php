a3893281859bb064db0c8adc7a44bffac089c987
Suada <suada.kumrija@fshnstudent.info>
2020-05-20 20:43:30 +0200

    feature : Krijimi i dashboard te adminit.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -42,12 +42,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
 
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Users <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Orders</a>
-            </li>
+
         </ul>
         <form class="form-inline my-2 my-lg-0">
             <ul class="navbar-nav mr-auto">
@@ -57,7 +52,6 @@
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="#">Profile</a>
-                        <a class="dropdown-item" href="#">Settings</a>
                         <a class="dropdown-item" href="{{ url('/logout') }}">Log Out</a>
                     </div>
                 </li>
